1cf410aaa329d21117171eb9a59e083a83bc8fa1
liph <45155620+liphx@users.noreply.github.com>
2020-08-13 20:13:22 +0800

    Update BigInteger.h

--- Algorithm/BigInteger.h
+++ Algorithm/BigInteger.h
@@ -17,6 +17,7 @@ public:
     BigInteger operator-(const BigInteger &bi) const;
     BigInteger operator*(const BigInteger &bi) const;
     BigInteger operator/(const BigInteger &bi) const;
+    BigInteger operator%(const BigInteger &bi) const;
 
     bool operator==(const BigInteger &bi) const;
     bool operator<(const BigInteger &bi) const;
@@ -28,6 +29,9 @@ public:
     std::string toString() const; // 转成字符串，包含符号
     ~BigInteger();
     friend std::ostream &operator<<(std::ostream &out, const BigInteger &bi);
+
+    static BigInteger gcd(const BigInteger &a, const BigInteger &b); // 最大公因数
+    static BigInteger lcm(const BigInteger &a, const BigInteger &b); // 最小公倍数
 private:
     int sign;           // 符号位，1表示正数，-1表示负数，0代表0
     std::string data;   // 以字符串形式存储大整数，其中不包含符号位，有效的格式为[1-9][0-9]* | 0
