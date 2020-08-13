0d5cc9d16b3c3a8ec7a41561b9437012853997dc
liph <45155620+liphx@users.noreply.github.com>
2020-08-13 14:53:48 +0800

    Update BigInteger.h

--- Algorithm/BigInteger.h
+++ Algorithm/BigInteger.h
@@ -11,24 +11,25 @@ public:
     BigInteger(std::string str);
     BigInteger(const BigInteger &bi);
     BigInteger& operator=(const BigInteger &bi);
-    BigInteger& operator+(const BigInteger &bi);
-    BigInteger& operator-(const BigInteger &bi);
-    BigInteger operator-();
-    BigInteger& operator*(const BigInteger &bi);
-    BigInteger& operator/(const BigInteger &bi);
-    bool operator==(const BigInteger &bi);
-    bool operator<(const BigInteger &bi);
-    bool operator<=(const BigInteger &bi);
-    bool operator!=(const BigInteger &bi);
-    bool operator>(const BigInteger &bi);
-    bool operator>=(const BigInteger &bi);
 
-    std::string toString() const;
+    BigInteger operator-() const;
+    BigInteger operator+(const BigInteger &bi) const;
+    BigInteger operator-(const BigInteger &bi) const;
+    BigInteger operator*(const BigInteger &bi) const;
+
+    bool operator==(const BigInteger &bi) const;
+    bool operator<(const BigInteger &bi) const;
+    bool operator>(const BigInteger &bi) const;
+    bool operator!=(const BigInteger &bi) const;
+    bool operator<=(const BigInteger &bi) const;
+    bool operator>=(const BigInteger &bi) const;
+
+    std::string toString() const; // 转成字符串，包含符号
     ~BigInteger();
     friend std::ostream &operator<<(std::ostream &out, const BigInteger &bi);
 private:
-    int getSign() const;
-    std::string data;
+    int sign;           // 符号位，1表示正数，-1表示负数，0代表0
+    std::string data;   // 以字符串形式存储大整数，其中不包含符号位，有效的格式为[1-9][0-9]* | 0
 };
 
 #endif
