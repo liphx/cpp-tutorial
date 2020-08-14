932f353bf785762916a29ca195b9fe341f6c5710
liph <45155620+liphx@users.noreply.github.com>
2020-08-14 15:48:53 +0800

    Update queue.cpp

--- STL/queue.cpp
+++ STL/queue.cpp
@@ -6,17 +6,19 @@ using namespace std;
 int main()
 {
     queue<int> data1;
-    queue<int> data2 {data1};
-    queue<int,list<int> > data3;
+    queue<int> data2 { data1 };
+    queue<int, list<int> > data3;
 
     data1.push(0);
     data1.push(1);
-    cout<<data1.front()<<endl;
-    cout<<data1.back()<<endl;
-    cout<<data1.size()<<endl;
-    cout<<data1.empty()<<endl;
+    data1.push(2);
+    cout << data1.front() << endl; // 0
+    cout << data1.back() << endl;  // 2
+    cout << data1.size() << endl;  // 3
+    cout << data1.empty() << endl; // 0
     data1.pop();
-    cout<<data1.front()<<endl;
+    cout << data1.front() << endl; // 1
+    cout << data1.back() << endl;  // 2
 
     return 0;
 }
