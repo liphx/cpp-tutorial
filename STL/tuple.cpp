7bd4dec35718a6a7f0dd8bc354f6874748799794
liph <45155620+liphx@users.noreply.github.com>
2020-08-14 16:08:18 +0800

    Update tuple.cpp

--- STL/tuple.cpp
+++ STL/tuple.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    auto t1 = make_tuple(string{"123"}, int{10} , bool {false});
-    tuple<string,string> t2 {"123", "321"};
-    auto t3 {t2};
-    cout<< (t3==t2) <<endl;
+    auto t1 = make_tuple(string { "123" }, int { 10 }, bool { false });
+    tuple<string, string> t2 { "123", "321" };
+    auto t3 { t2 };
+    cout << (t3 == t2) << endl; // 1
     get<0>(t3) = "124";
-    cout<< (t3>t2) <<endl;
-    cout<<get<int>(t1)<<endl; //要求t1中只有一个int
+    cout << (t3 > t2) << endl;    // 1
+    cout << get<int>(t1) << endl; // 10，要求t1中只有一个int
 
     return 0;
 }
